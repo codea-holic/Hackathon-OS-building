566b3aafbb31d2c8cc00f0eb3f50ed8b6c4f093c
Ganesh <krganesh1611@gmail.com>
2021-11-01 09:40:27 +0530

    Replaced all the icon with images

--- main.go
+++ main.go
@@ -10,70 +10,118 @@ import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
 var myApp fyne.App = app.New()
-var myWindow fyne.Window = myApp.NewWindow("My OS");
+var myWindow fyne.Window = myApp.NewWindow("My Virtual OS")
 var btn1 fyne.Widget
 var btn2 fyne.Widget
 var btn3 fyne.Widget
 var btn4 fyne.Widget
 var DeskBtn fyne.Widget
-var img fyne.CanvasObject;
+var img fyne.CanvasObject
 var panelContent *fyne.Container
 
-func main(){
+func main() {
 	myApp.Settings().Theme()
 	img = canvas.NewImageFromFile("C:\\Users\\Ganesh\\Desktop\\VirtualOS\\Desktop.jpg")
-	
-	iconFile , err := os.Open(".\\Icons\\home.png")
+
+	sunIcon, err := os.Open(".\\Icons\\sun.png")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	r := bufio.NewReader(iconFile)
+	sunReader := bufio.NewReader(sunIcon)
 
-	b, err := ioutil.ReadAll(r)
+	sun, err := ioutil.ReadAll(sunReader)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	btn1 = widget.NewButtonWithIcon("Weather App", theme.InfoIcon(), func() {
+	btn1 = widget.NewButtonWithIcon("Weather App", fyne.NewStaticResource("icon", sun), func() {
 		weatherApp(myWindow)
 	})
 
-	btn2 = widget.NewButtonWithIcon("Calculator", theme.ContentAddIcon(), func ()  {
+	calcIcon, err := os.Open(".\\Icons\\calculator.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	calcReader := bufio.NewReader(calcIcon)
+
+	calc, err := ioutil.ReadAll(calcReader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	btn2 = widget.NewButtonWithIcon("Calculator", fyne.NewStaticResource("icon", calc), func() {
 		showCalc()
 	})
 
-	btn3 = widget.NewButtonWithIcon("Gallery", theme.StorageIcon(), func ()  {
+	photoIcon , err := os.Open(".\\Icons\\photos.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	photoReader := bufio.NewReader(photoIcon)
+
+	photo, err := ioutil.ReadAll(photoReader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	btn3 = widget.NewButtonWithIcon("Gallery", fyne.NewStaticResource("icon", photo), func() {
 		GalleryApp(myWindow)
 	})
 
-	btn4 = widget.NewButtonWithIcon("Text Editor", theme.DocumentIcon(), func ()  {
+	textIcon , err := os.Open(".\\Icons\\text.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	textReader := bufio.NewReader(textIcon)
+
+	text, err := ioutil.ReadAll(textReader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	btn4 = widget.NewButtonWithIcon("Text Editor", fyne.NewStaticResource("icon", text), func() {
 		textEditor()
 	})
 
-	DeskBtn = widget.NewButtonWithIcon("Home", fyne.NewStaticResource("icon", b), func () {
+	homeIcon, err := os.Open(".\\Icons\\home.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := bufio.NewReader(homeIcon)
+
+	home, err := ioutil.ReadAll(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	DeskBtn = widget.NewButtonWithIcon("Home", fyne.NewStaticResource("icon", home), func() {
 		myWindow.SetContent(container.NewBorder(panelContent, nil, nil, nil, img))
 	})
 
-	panelContent = container.NewVBox((container.NewGridWithColumns(5, DeskBtn, btn1, btn2, btn3, btn4)));
+	panelContent = container.NewVBox((container.NewGridWithColumns(1,
+		container.NewGridWithColumns(3, DeskBtn, btn1, btn2),
+		container.NewGridWithColumns(3, btn3, btn4),
+	)))
 
-	myWindow.Resize(fyne.NewSize(1280, 720));
+	myWindow.Resize(fyne.NewSize(1280, 720))
 
-	myWindow.CenterOnScreen();
+	myWindow.CenterOnScreen()
 
 	myWindow.SetContent(
 		container.NewBorder(panelContent, nil, nil, nil, img),
 	)
 
-
 	myWindow.ShowAndRun()
 }
-
 
 /*
 iconFile, err := os.Open("C:/path/to/res/icon.png")
